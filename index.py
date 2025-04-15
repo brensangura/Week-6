d6692838c52fdb9f0bc7af2f40ef30a37cc8504e
brensangura <sangura.bren@gmail.com>
2025-04-15 15:50:33 +0300

    pandas

---
+++ index.py
@@ -0,0 +1,12 @@
+import math
+# from math import sqrt
+import random
+print(math.sqrt(17))
+print("Power of 2^3:", math.pow(2, 9.5))
+
+# print a Random Number
+
+print("Random Number:", random.randint(1, 12))
+
+print("Random JsMammie is: ", random.choice(
+    ["Joy", "Naomi", "Judy", "Lynne", "Mercy"]))
---
+++ pandas_example.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [24, 30, 22],
+    'Score': [85, 90, 95]
+}
+
+df  = pd.DataFrame(data)
+# print("DataFrame:\n", df)
+
+# print("Names:", df['Age'])
+print(df[df['Score'] >= 90])
+
