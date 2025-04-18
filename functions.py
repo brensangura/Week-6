0f73f446a24b5ee5d5b43a822a9e570818d3a71a
brensangura <sangura.bren@gmail.com>
2025-04-18 13:34:22 +0300

    er

---
+++ functions.py
@@ -0,0 +1,13 @@
+def greet(first_name, last_name):
+    print(f"Hello {first_name}, {last_name}!")
+    print("This program allows you to manage a virtual pet.")
+
+
+greet("Mosh", "Hamedani")
+# The greet function is a simple function that prints a welcome message to the user.
+
+greet("John", "Doe")
+# The greet function is a simple function that prints a welcome message to the user.
+
+greet("Brian", "Sangura")
+# The greet function is a simple function that prints a welcome message to the user.
---
+++ numbers.py
@@ -0,0 +1,6 @@
+count = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        count += 1
+        print("This is an even number.")
+        print(number)
