931bf64741a133d10b42cb5999b3c5604106349b
brensangura <sangura.bren@gmail.com>
2025-04-21 22:58:34 +0300

    happy

---
+++ practice.py
@@ -0,0 +1,22 @@
+import math
+print(math.sqrt(16))
+
+import math
+print(math.sqrt(96))
+
+import math
+print(math.sqrt(100))
+
+import math
+print(math.sqrt(900))
+
+import math
+print(math.sqrt(1000))
+
+import math
+print(math.sqrt(10000))
+
+import math
+print("Square root of 36:", math.sqrt(36))
+print("Sine of 90 degrees:", math.sin(math.radians(90)))
+print("Power of 2^3:", math.pow(2, 3))
